9717551aa06c3f0396aa62ba6dff52dc4565626f
Aryaholmukhe <68239007+Aryaholmukhe@users.noreply.github.com>
2021-02-23 21:02:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground;
-
 function preload()
 {
-	helicopterIMG=loadImage("helicopter.png");
-	packageIMG=loadImage("package.png");
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
 function setup() {
@@ -18,11 +17,11 @@ function setup() {
 	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG);
+	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG);
+	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
 	groundSprite=createSprite(width/2, height-35, width,10);
@@ -32,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1}, {isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -49,16 +48,16 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
   drawSprites();
- 
+  keyPressed()
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-     Matter.Body.setStatic(packageBody, {isStatic:false})
+	Matter.Body.setStatic(packageBody, false);
     
   }
 }
